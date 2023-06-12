a2d9fd97ec6be5e43bcdeec689e8c29429412a83
Hiếu Thuận Nguyễn <nguyenthuan2704@gmail.com>
2023-06-12 20:20:28 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          Hi there - How are you 
+          I'm fine , thanks.bla bla bla
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
